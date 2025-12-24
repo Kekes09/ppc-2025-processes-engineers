f57cbdbb4ff78560aa44ceb70454a4accbc5721d
luch2001lkop@gmail.com <luch2001lkop@gmail.com>
2025-12-24 18:40:16 +0300

    restartmainbr2

--- tasks/luchnikov_e_gener_transm_from_all_to_one_gather/mpi/include/ops_mpi.hpp
+++ tasks/luchnikov_e_gener_transm_from_all_to_one_gather/mpi/include/ops_mpi.hpp
@@ -10,13 +10,14 @@ class LuchnikovETransmFrAllToOneGatherMPI : public BaseTask {
   static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
     return ppc::task::TypeOfTask::kMPI;
   }
+  
   explicit LuchnikovETransmFrAllToOneGatherMPI(const InType &in);
 
  private:
   bool ValidationImpl() override;
   bool PreProcessingImpl() override;
   bool RunImpl() override;
-  bool PostProcessingImpl() override;
+  bool PostProcessingImpl() override; 
 };
 
 }  // namespace luchnikov_e_gener_transm_from_all_to_one_gather
